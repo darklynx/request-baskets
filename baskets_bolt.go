533f58a830fa3af77500339da5a56ff62890b78d
Vladimir L <vladimir_l@gmx.net>
2016-01-06 19:03:45 +0100

    bolt: implemented database interface, partially implemented basket interface

--- baskets_bolt.go
+++ baskets_bolt.go
@@ -14,12 +14,36 @@ var (
 	KEY_FORWARD_URL = []byte("url")
 	KEY_CAPACITY    = []byte("capacity")
 	KEY_TOTAL_COUNT = []byte("total")
+	KEY_COUNT       = []byte("count")
 	KEY_REQUESTS    = []byte("requests")
 )
 
 /// Basket interface ///
 
 type boltBasket struct {
+	db   *bolt.DB
+	name string
+}
+
+func (basket *boltBasket) Config() BasketConfig {
+	config := BasketConfig{}
+
+	basket.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(basket.name))
+		if b != nil {
+			url := b.Get(KEY_FORWARD_URL)
+			cap := b.Get(KEY_CAPACITY)
+
+			config.ForwardUrl = string(url[:])
+			config.Capacity, _ = strconv.Atoi(string(cap[:]))
+		} else {
+			log.Printf("Failed to locate bucket: %s", basket.name)
+		}
+
+		return nil
+	})
+
+	return config
 }
 
 /// BasketsDatabase interface ///
@@ -46,7 +70,8 @@ func (bdb *boltDatabase) Create(name string, config BasketConfig) (BasketAuth, e
 		b.Put(KEY_FORWARD_URL, []byte(config.ForwardUrl))
 		b.Put(KEY_CAPACITY, []byte(strconv.Itoa(config.Capacity)))
 		b.Put(KEY_TOTAL_COUNT, []byte("0"))
-		b.CreateBucketIfNotExists(KEY_REQUESTS)
+		b.Put(KEY_COUNT, []byte("0"))
+		b.CreateBucket(KEY_REQUESTS)
 
 		return err
 	})
@@ -60,11 +85,73 @@ func (bdb *boltDatabase) Create(name string, config BasketConfig) (BasketAuth, e
 	return auth, nil
 }
 
+func (bdb *boltDatabase) Get(name string) *boltBasket {
+	err := bdb.db.View(func(tx *bolt.Tx) error {
+		if tx.Bucket([]byte(name)) != nil {
+			return nil
+		} else {
+			return fmt.Errorf("No basket found: %s", name)
+		}
+	})
+
+	if err != nil {
+		log.Print(err)
+		return nil
+	} else {
+		return &boltBasket{bdb.db, name}
+	}
+}
+
+func (bdb *boltDatabase) Delete(name string) {
+	err := bdb.db.Update(func(tx *bolt.Tx) error {
+		return tx.DeleteBucket([]byte(name))
+	})
+
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func (bdb *boltDatabase) Size() int {
+	// TODO : introduce bucket with statistics (e.g. "/stats", or ".stats"), see https://github.com/boltdb/bolt/issues/276
+	size := 0
+
+	bdb.db.View(func(tx *bolt.Tx) error {
+		cur := tx.Cursor()
+		for key, _ := cur.First(); key != nil; key, _ = cur.Next() {
+			size++
+		}
+		return nil
+	})
+
+	return size
+}
+
+func (bdb *boltDatabase) GetNames(max int, skip int) BasketNamesPage {
+	last := skip + max
+	page := BasketNamesPage{make([]string, 0, max), 0, false}
+
+	bdb.db.View(func(tx *bolt.Tx) error {
+		cur := tx.Cursor()
+		for key, _ := cur.First(); key != nil; key, _ = cur.Next() {
+			if page.Count >= skip && page.Count < last {
+				page.Names[len(page.Names)] = string(key[:])
+			} else if page.Count >= last {
+				page.HasMore = true
+			}
+			page.Count++
+		}
+		return nil
+	})
+
+	return page
+}
+
 func (bdb *boltDatabase) Release() {
 	log.Printf("Releasing bolt database resources")
 	err := bdb.db.Close()
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 }
 
