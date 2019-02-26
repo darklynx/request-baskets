5b4d2fa7662611c47656f607e26e82dcccff88ba
Vladimir Lyubitelev <vladimir.lyubitelev@optile.net>
2019-02-26 01:21:28 +0100

    more tests for SQL issues during different opertaions with baskets

--- baskets_sql_test.go
+++ baskets_sql_test.go
@@ -7,8 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// These are mostly error tests for SQL baskets database when the connection is wron or broken
-// That is why this tests cannot be refred as tests related to the cirtain driver (PostgreSQL, MySQL, etc.)
+// These are mostly error tests for SQL baskets database when the connection is wrong or broken
+// That is why this tests cannot be referred as tests related to the cirtain driver (PostgreSQL, MySQL, etc.)
 
 func TestSQLDatabase_Create_InvalidDriver(t *testing.T) {
 	assert.Nil(t, NewSQLDatabase("invalid_driver://user@localhost"), "expected to fail and return nil")
@@ -20,6 +20,49 @@ func TestSQLDatabase_Create_NoDriver(t *testing.T) {
 
 func TestSQLDatabase_Create_FailedPing(t *testing.T) {
 	assert.Nil(t, NewSQLDatabase("postgres://user@localhost:8907/baskets"), "expected to fail and return nil")
+}
+
+func TestSQLDatabase_Get_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basketsDb := sqlDatabase{db: sqldb, dbType: "postgres"}
+	basket := basketsDb.Get("anybasket")
+	assert.Nil(t, basket, "basket is not expected")
+}
+
+func TestSQLDatabase_Delete_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basketsDb := sqlDatabase{db: sqldb, dbType: "postgres"}
+	basketsDb.Delete("anybasket")
+	// TODO: find out how to capture the log output for validation
+}
+
+func TestSQLDatabase_GetNames_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basketsDb := sqlDatabase{db: sqldb, dbType: "postgres"}
+	page := basketsDb.GetNames(10, 0)
+	if assert.NotNil(t, page, "page object with names is expected") {
+		assert.Equal(t, 0, page.Count)
+		assert.False(t, page.HasMore)
+		assert.Empty(t, page.Names)
+	}
+}
+
+func TestSQLDatabase_FindNames_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basketsDb := sqlDatabase{db: sqldb, dbType: "postgres"}
+	page := basketsDb.FindNames("a", 10, 0)
+	if assert.NotNil(t, page, "page object with names is expected") {
+		assert.False(t, page.HasMore)
+		assert.Empty(t, page.Names)
+	}
 }
 
 func TestSQLBasket_InvalidBasket(t *testing.T) {
@@ -50,4 +93,108 @@ func TestSQLBasket_InvalidBasket(t *testing.T) {
 	findPage := basket.FindRequests("", "any", 10, 0)
 	assert.NotNil(t, findPage, "requests page is expected")
 	assert.Equal(t, 0, len(findPage.Requests), "wrong number of Requests in page")
+}
+
+func TestSQLBasket_Update_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basket := sqlBasket{db: sqldb, dbType: "postgres", name: "anybasket"}
+	basket.Update(BasketConfig{Capacity: 10})
+	// TODO: find out how to capture the log output for validation
+}
+
+func TestSQLBasket_Authorize_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basket := sqlBasket{db: sqldb, dbType: "postgres", name: "anybasket"}
+	assert.False(t, basket.Authorize("token"))
+}
+
+func TestSQLBasket_GetResponse_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basket := sqlBasket{db: sqldb, dbType: "postgres", name: "anybasket"}
+	assert.Nil(t, basket.GetResponse("GET"))
+}
+
+func TestSQLBasket_Add_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basket := sqlBasket{db: sqldb, dbType: "postgres", name: "anybasket"}
+	basket.Add(createTestPOSTRequest("http://localhost/anybasket", "Hellow world", "text/plain"))
+	// TODO: find out how to capture the log output for validation
+}
+
+func TestSQLBasket_Clear_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basket := sqlBasket{db: sqldb, dbType: "postgres", name: "anybasket"}
+	basket.Clear()
+	// TODO: find out how to capture the log output for validation
+}
+
+func TestSQLBasket_GetRequests_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basket := sqlBasket{db: sqldb, dbType: "postgres", name: "anybasket"}
+	page := basket.GetRequests(10, 0)
+	if assert.NotNil(t, page, "page object with requests is expected") {
+		assert.Equal(t, 0, page.Count)
+		assert.Equal(t, 0, page.TotalCount)
+		assert.False(t, page.HasMore)
+		assert.Empty(t, page.Requests)
+	}
+}
+
+func TestSQLBasket_FindRequests_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basket := sqlBasket{db: sqldb, dbType: "postgres", name: "anybasket"}
+	page := basket.FindRequests("q", "any", 10, 0)
+	if assert.NotNil(t, page, "page object with requests is expected") {
+		assert.False(t, page.HasMore)
+		assert.Empty(t, page.Requests)
+	}
+}
+
+// SQL errors tests in private methods
+
+func TestSQLDatabase_getTopBaskets_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	defer sqldb.Close()
+
+	basketsDb := sqlDatabase{db: sqldb, dbType: "postgres"}
+	assert.Empty(t, basketsDb.getTopBaskets("SELECT x FROM FROM", 10), "no baskets are expected")
+}
+
+func TestSQLDatabase_getInt_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	defer sqldb.Close()
+
+	basketsDb := sqlDatabase{db: sqldb, dbType: "postgres"}
+	assert.Equal(t, 12, basketsDb.getInt("SELECT count(x) FROM FROM", 12), "default value is expected if SQL error occurs")
+}
+
+func TestSQLBasket_getInt_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	defer sqldb.Close()
+
+	basket := sqlBasket{db: sqldb, dbType: "postgres", name: "anybasket"}
+	assert.Equal(t, 42, basket.getInt("SELECT count(x) FROM FROM", 42), "default value is expected if SQL error occurs")
+}
+
+func TestSQLBasket_applyLimit_SQLError(t *testing.T) {
+	sqldb, _ := sql.Open("postgres", pgTestConnection)
+	sqldb.Close()
+
+	basket := sqlBasket{db: sqldb, dbType: "postgres", name: "anybasket"}
+	basket.applyLimit(-1)
+	// TODO: find out how to capture the log output for validation
 }
